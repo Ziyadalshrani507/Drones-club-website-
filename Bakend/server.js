782a7d757566ef27aef00f02ee49c9eb4cafd24c
ahmadammarjr <110498902+KamenYaiba@users.noreply.github.com>
2025-03-01 07:19:28 +0300

    switch to ejs for dynamic image injection and to add a common header and footer

--- Bakend/server.js
+++ Bakend/server.js
@@ -3,12 +3,16 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+
 // Serve static files from the "Frontend" directory
 app.use(express.static(path.join(__dirname, '../Frontend')));
 
-// Route to serve the index.html file
+// Route to serve the index.ejs file
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../Frontend/HTML', 'index.html'));
+    res.render(path.join(__dirname, '../Frontend/VIEWS', 'index'));
 });
 
 app.listen(PORT, () => {
