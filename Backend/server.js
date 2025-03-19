3be0d4c196976e2be4a8f94bcfba46c70f1aa015
ahmadammarjr <110498902+KamenYaiba@users.noreply.github.com>
2025-03-19 10:21:40 +0300

    add joinevent page to server.js

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
 
@@ -15,46 +16,44 @@ app.get('/', (req, res) => {
     res.render(path.join(__dirname, '../Frontend/VIEWS', 'index'));
 });
 
-// Route to serve the join.ejs file
+
 app.get('/joinus', (req, res) => {
-    res.render(path.join(__dirname, '../Frontend/VIEWS', 'join'));
+  res.render(path.join(__dirname, '../Frontend/VIEWS', 'join'));
 });
 
-// Route to serve the events.ejs file
-const eventImgDir = path.join(__dirname, '../Frontend/images/eventimages');
+
+app.get('/eventform', (req, res) => {
+  res.render(path.join(__dirname, '../Frontend/VIEWS', 'eventform'));
+});
+
+
+// Route to serve the index.ejs file
+const eventImgDir = "Frontend/images/eventimages";
 app.get('/events', (req, res) => {
     fs.readdir(eventImgDir, (err, files) => {
-        if (err) {
-            console.log(err);
-            return res.status(500).send('Error loading images');
-        }
-        res.render(path.join(__dirname, '../Frontend/VIEWS', 'events'), { images: files });
+      if (err) {
+        console.log(err);
+        console.log(files)
+        return res.status(500).send('Error loading images');
+      }
+      res.render(path.join(__dirname, '../Frontend/VIEWS', 'events'), { images: files }); 
     });
-});
+  });
 
-// Route to serve the photogallery.ejs file
-const photoGalleryDir = path.join(__dirname, '../Frontend/images/photogallery');
+
+
+const imgGalleryDir = "Frontend/images/photogallery";
 app.get('/photogallery', (req, res) => {
-    fs.readdir(photoGalleryDir, (err, files) => {
-        if (err) {
-            console.log(err);
-            return res.status(500).send('Error loading images');
-        }
-        res.render(path.join(__dirname, '../Frontend/VIEWS', 'photogallery'), { images: files });
+    fs.readdir(imgGalleryDir, (err, files) => {
+      if (err) {
+        console.log(err);
+        console.log(files)
+        return res.status(500).send('Error loading images');
+      }
+      res.render(path.join(__dirname, '../Frontend/VIEWS', 'photogallery'), { images: files }); 
     });
-});
+  });
 
-// Route to serve the about.ejs file
-app.get('/about', (req, res) => {
-    res.render(path.join(__dirname, '../Frontend/VIEWS', 'about'));
-});
-
-// Route to serve the team.ejs file
-app.get('/team', (req, res) => {
-    res.render(path.join(__dirname, '../Frontend/VIEWS', 'team'));
-});
-
-// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
